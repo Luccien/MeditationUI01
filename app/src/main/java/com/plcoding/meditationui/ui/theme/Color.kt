2810183e29580a38780c28959e030b292577b737
Lucien <luccien>
2021-08-22 14:08:19 +0200

    new theme code and resources

--- app/src/main/java/com/plcoding/meditationui/ui/theme/Color.kt
+++ app/src/main/java/com/plcoding/meditationui/ui/theme/Color.kt
@@ -2,6 +2,33 @@ package com.plcoding.meditationui.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+
+
+
+//////////////////////////// NEW
+val Blue300 = Color(0xFF64B5F6)
+val Blue400 = Color(0xFF42A5F5)
+val Blue500 = Color(0xFF2196F3)
+val Blue600 = Color(0xFF1E88E5)
+val Blue700 = Color(0xFF1976D2)
+val Blue800 = Color(0xFF1565C0)
+
+val Teal300 = Color(0xFF1AC6FF)
+
+val Grey1 = Color(0xFFF2F2F2)
+
+val Black1 = Color(0xFF222222)
+val Black2 = Color(0xFF000000)
+
+val RedErrorDark = Color(0xFFB00020)
+val RedErrorLight = Color(0xFFEF5350)
+/////////////////////////// NEW
+
+
+
+
+
+//////////////////////////////// /// TODO delete old code OLD /////////////
 val LightBlue = Color(0xFFBAC7FF)
 
 val TextWhite = Color(0xffeeeeee)
@@ -22,6 +49,10 @@ val LightGreen3 = Color(0xff11d79b)
 val BlueViolet1 = Color(0xffaeb4fd)
 val BlueViolet2 = Color(0xff9fa5fe)
 val BlueViolet3 = Color(0xff8f98fd)
+///////////////////////////////////////////// old ////////////
+
+
+
 
 
 /*
